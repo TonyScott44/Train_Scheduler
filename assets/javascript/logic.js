efbb100d843739783347f658235ddae2bc36bb18
tonyscott44 <acsii@me.com>
2020-12-12 01:33:03 -0500

    Investigate bug with arrival time

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-var config = {
+const config = {
     apiKey: "AIzaSyDoUSZAFPRE0CwEe3X2B1DcuRNH34KVeds",
     authDomain: "practice-9f0f7.firebaseapp.com",
     databaseURL: "https://practice-9f0f7.firebaseio.com",
@@ -10,30 +10,29 @@ var config = {
 firebase.initializeApp(config);
 
 // Get a reference to the database service
-var database = firebase.database();
+const database = firebase.database();
 
 // On Click
 $("#add-user").on("click", function() {
 
     // Get the input values
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#train-destination").val().trim();
-    var frequency = $("#train-frequency").val().trim();
-    var firstTrain = $("#first-train").val().trim();
-    var oTrainTime = moment(firstTrain, "hh:mm").subtract(1, "years");
-    var mTrainTime = moment(firstTrain, "hh:mm");
-    var eTrainTime = Object.values(oTrainTime)[1];
-    var iTrainTime = eTrainTime.substring(0,5);
-
-    var difTime = moment().diff(moment(oTrainTime), "minutes");
-    var rmndr = difTime % frequency;
-    var minAwy = frequency - rmndr;
-    var nxtArvl = moment().add(minAwy, "minutes");
-    var eNxtArvl = Object.values(nxtArvl)[4].toTimeString();
-    var nextArrival = eNxtArvl.substring(0,5);
-    var maxMoment = moment.max(moment(), oTrainTime);
-    var eMaxMoment = Object.values(maxMoment)[4].toTimeString();
-    var maxTime = eMaxMoment.substring(0,5);
+    let trainName = $("#train-name").val().trim(),
+        destination = $("#train-destination").val().trim(),
+        frequency = $("#train-frequency").val().trim(),
+        firstTrain = $("#first-train").val().trim(),
+        oTrainTime = moment(firstTrain, "hh:mm").subtract(1, "years"),
+        mTrainTime = moment(firstTrain, "hh:mm"),
+        eTrainTime = Object.values(oTrainTime)[1],
+        iTrainTime = eTrainTime.substring(0,5),
+        difTime = moment().diff(moment(oTrainTime), "minutes"),
+        rmndr = difTime % frequency,
+        minAwy = frequency - rmndr,
+        nxtArvl = moment().add(minAwy, "minutes"),
+        eNxtArvl = Object.values(nxtArvl)[4].toTimeString(),
+        nextArrival = eNxtArvl.substring(0,5),
+        maxMoment = moment.max(moment(), oTrainTime),
+        eMaxMoment = Object.values(maxMoment)[4].toTimeString(),
+        maxTime = eMaxMoment.substring(0,5);
 
     if (maxTime > iTrainTime) {
 
@@ -67,12 +66,11 @@ $("#add-user").on("click", function() {
 database.ref().on("child_added", function(childSnapshot) {
 
     // Log everything that's coming out of snapshot
-    console.log(childSnapshot.val().tName);
-    console.log(childSnapshot.val().dest);
-    console.log(childSnapshot.val().nxtArr);
-    console.log(childSnapshot.val().freq);
-    console.log(childSnapshot.val().minAwy);
-
+    console.log(`Train Name: ${childSnapshot.val().tName}`);
+    console.log(`Destination: ${childSnapshot.val().dest}`);
+    console.log(`Next Arrival: ${childSnapshot.val().nxtArr}`);
+    console.log(`Frequency ${childSnapshot.val().freq}`);
+    console.log(`Minutes Away: ${childSnapshot.val().minAwy}`);
 
     // full list of items to the well
     $("#trainData").append("<tr> <td> " + childSnapshot.val().tName +
